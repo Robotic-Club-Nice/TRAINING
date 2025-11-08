e9340216a62ed7b6e7c899433ab3259b14f7f085
ChrisG-L <christophe.gerardin@epitech.eu>
2025-11-08 18:16:25 +0100

    fix: set DRIVE_LOOP_HZ to same as VOITURE_ROBOCAR

--- main_parts/config.py
+++ main_parts/config.py
@@ -3,7 +3,7 @@ import os
 CAR_PATH = os.path.dirname(os.path.realpath(__file__))
 DATA_PATH = os.path.join(CAR_PATH, '../data')
 MODELS_PATH = os.path.join(CAR_PATH, '../models')
-DRIVE_LOOP_HZ = 20
+DRIVE_LOOP_HZ = 30
 
 IMAGE_W = 160
 IMAGE_H = 120
